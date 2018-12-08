fb21b80789be028b3005a22e4c615b7c70a4c0df
leolleocomp <leolleo.comp@gmail.com>
2018-12-08 14:32:20 -0300

    Add componente de monitoramento do status da irrigação

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -6,19 +6,34 @@
   >
     <v-layout wrap>
       <v-flex
-        sm6
-        xs12
-        md6
-        lg3
+        lg12
       >
-        <material-stats-card
-          color="green"
-          icon="mdi-arrow-right-bold-box"
-          title="Revenue"
-          value="$34,245"
-          sub-icon="mdi-calendar"
-          sub-text="Last 24 Hours"
-        />
+        <v-card>
+          <v-card-title>
+            Estado Atual da Irrigação
+          </v-card-title>
+          <v-card-text
+            d-flex
+          >
+            <p md6>Sensor da linha 2 <v-chip color="blue lighten-4">OK</v-chip></p>
+            <v-progress-linear
+              indeterminate
+              color="blue lighten-4"
+            />
+
+            <p md6>Sensor da linha 3 <v-chip color="red">Abaixo do recomendado</v-chip></p>
+            <v-progress-linear
+              color="red"
+              indeterminate
+            />
+
+            <p md6>Sensor da linha 4 <v-chip color="red">Acima do normal</v-chip></p>
+            <v-progress-linear
+              color="red"
+              indeterminate
+            />
+          </v-card-text>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
