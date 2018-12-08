586e13e9b4e849c587fb1cbb4a246af198158509
jedsonmelo <jedsonmeloc@gmail.com>
2018-12-08 17:52:32 -0300

    Outra abordagem de dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -5,34 +5,41 @@
     grid-list-xl
   >
     <v-layout wrap>
-      <v-flex
-        sm6
-        xs12
-        md6
-      >
-        <v-card>
-          <v-card-title primary-title>
-            <v-container fluid>
-              <div
-                class="headline"
-              >
-                Sensor da linha 1
-              </div>
+        <v-flex
+            sm12
+            xs12
+            md12
+        >
+            <v-card>
+                <v-list>
+                    <v-list-tile @click="clicked()">
+                        <v-list-tile-action>
+                        <v-icon color="red">mdi-alarm-light</v-icon>
 
-              <v-progress-linear
-                indeterminate
-              />
-            </v-container>
-          </v-card-title>
-          <v-card-actions>
-            <v-btn
-              flat
-              color="success"
-            >
-              Detalhes
-            </v-btn>
-          </v-card-actions>
-        </v-card>
+                        </v-list-tile-action>
+                            <v-list-tile-title
+                                color="danger"
+                            >
+                                Sensor da linha 1
+                            </v-list-tile-title>
+                        <div class="d-flex">
+                            <v-tooltip
+                                top
+                                content-class="top"
+                            >
+                                <v-btn
+                                    slot="activator"
+                                    class="v-btn--simple"
+                                    icon
+                                >
+                                    <v-icon color="grey">mdi-map-marker</v-icon>
+                                </v-btn>
+                                <span>Localização</span>
+                            </v-tooltip>
+                        </div>
+                    </v-list-tile>
+                </v-list>
+            </v-card>
       </v-flex>
     </v-layout>
   </v-container>
@@ -40,5 +47,21 @@
 
 <script>
 export default {
+    data () {
+        return {
+            tabs: 0,
+            list: {
+                0: false,
+                1: false,
+                2: false
+            }
+        }
+    },
+
+    methods: {
+        clicked() {
+            console.log('cliquei');
+        }
+    }
 }
 </script>
