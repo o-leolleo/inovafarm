ac06d73b1c7251d51af5d94c645b6029293f5fe6
leolleocomp <leolleo.comp@gmail.com>
2018-12-08 15:14:54 -0300

    Add layout basico de card de sensor

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -6,33 +6,32 @@
   >
     <v-layout wrap>
       <v-flex
-        lg12
+        sm6
+        xs12
+        md6
       >
         <v-card>
-          <v-card-title>
-            Estado Atual da Irrigação
+          <v-card-title primary-title>
+            <v-container fluid>
+              <div
+                class="headline"
+              >
+                Sensor da linha 1
+              </div>
+
+              <v-progress-linear
+                indeterminate
+              />
+            </v-container>
           </v-card-title>
-          <v-card-text
-            d-flex
-          >
-            <p md6>Sensor da linha 2 <v-chip color="blue lighten-4">OK</v-chip></p>
-            <v-progress-linear
-              indeterminate
-              color="blue lighten-4"
-            />
-
-            <p md6>Sensor da linha 3 <v-chip color="red">Abaixo do recomendado</v-chip></p>
-            <v-progress-linear
-              color="red"
-              indeterminate
-            />
-
-            <p md6>Sensor da linha 4 <v-chip color="red">Acima do normal</v-chip></p>
-            <v-progress-linear
-              color="red"
-              indeterminate
-            />
-          </v-card-text>
+          <v-card-actions>
+            <v-btn
+              flat
+              color="success"
+            >
+              Detalhes
+            </v-btn>
+          </v-card-actions>
         </v-card>
       </v-flex>
     </v-layout>
@@ -41,10 +40,5 @@
 
 <script>
 export default {
-  methods: {
-    complete (index) {
-      this.list[index] = !this.list[index]
-    }
-  }
 }
 </script>
